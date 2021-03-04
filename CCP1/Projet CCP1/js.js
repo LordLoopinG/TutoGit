2f3428726723c4eefb16585af3f68346f6b60807
sebastienpeugnet <sebastien.peugnet@gmail.com>
2021-03-04 13:52:53 +0100

    lecteur+navBarre+deconnexion

--- CCP1/Projet CCP1/js.js
+++ CCP1/Projet CCP1/js.js
@@ -49,6 +49,12 @@ $(document).ready(function () {
         controleInputRegister()
     })
 
+    //Deconnexion
+    $("#deconnex").click(function() {
+        sessionStorage.clear()
+        location.reload()
+    })
+
     //Controle des champs du formulaire d'inscription
     function controleInputRegister () {
         var inputRegister = false
@@ -152,11 +158,16 @@ $(document).ready(function () {
     }
     //Fin Login
 
+    //lecteur audio
+
+    //Fin LECTEUR AUDIO
+
     function uuidv4() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
           var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
           return v.toString(16);
         });
     }
+
 
 })
